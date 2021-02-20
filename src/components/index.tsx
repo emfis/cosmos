1510801454e8f57b49e7d76735afe0f4b134129e
Jerry <jeremiasz.mozgwa@jazzy.pro>
2021-02-20 01:29:57 +0100

    index added

---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+export {default as Alert} from "./Alert";
+export {default as App} from "./App";
+export {default as LocationSearch} from "./LocationSearch";
+export {default as Map} from "./Map";
+export {default as PageWrapper} from "./PageWrapper";
+export {default as TopBar} from "./TopBar";
+export {default as Image} from "./Image";
